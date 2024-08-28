dfe13fc7e93111a327f2450ef2d9e540c2925796
FRAMEWORK-1\a_lle <lleras.ag@gmail.com>
2024-08-28 18:33:13 -0400

    Fix snackbar functionality in base provider

--- lib/providers/base_provider.dart
+++ lib/providers/base_provider.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:voyagr_mobile/util/globals.dart';
 
 class BaseProvider extends ChangeNotifier {
   final BuildContext context;
 
   BaseProvider({required this.context});
 
-  void showError(String errorMsg) {
-    if (!context.mounted) return;
-
-    ScaffoldMessenger.of(context).showSnackBar(
+  void showInformation(String msg) {
+    Globals.globalKey.currentState?.showSnackBar(
       SnackBar(
-        content: Text('Error: ${errorMsg}'),
+        content: Text(msg),
+      )
+    );
+  }
+
+  void showError(String errorMsg) {
+    Globals.globalKey.currentState?.showSnackBar(
+      SnackBar(
+        content: Text('Error: $errorMsg'),
       )
     );
   }
